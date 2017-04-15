1693c2abbdd5373e26d561267a49519fc7bc0a67
Jussi Pakkanen <jpakkane@gmail.com>
2017-04-15 21:01:38 +0300

    Create widgets and show them.

--- main.cpp
+++ main.cpp
@@ -17,7 +17,34 @@
 
 #include<libssh/libssh.h>
 #include<vte/vte.h>
+#include<gtk/gtk.h>
+
+struct App {
+    GtkWidget *mainWindow;
+    GtkWidget *terminal;
+};
 
 int main(int argc, char **argv) {
+    struct App app;
+    GtkWidget *box;
+    GtkWidget *scrollBar;
+    GMainLoop *ml = g_main_loop_new(nullptr, FALSE);
+
+    gtk_init(&argc, &argv);
+    app.mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(app.mainWindow), "Unnamed SSH client");
+
+    app.terminal = vte_terminal_new();
+    vte_terminal_set_size(VTE_TERMINAL(app.terminal), 80, 25);
+
+    box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    scrollBar = gtk_scrollbar_new(GTK_ORIENTATION_VERTICAL, gtk_scrollable_get_vadjustment(GTK_SCROLLABLE(app.terminal)));
+    gtk_box_pack_start(GTK_BOX(box), app.terminal, TRUE, TRUE, 0);
+    gtk_box_pack_end(GTK_BOX(box), scrollBar, FALSE, TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(app.mainWindow), box);
+    gtk_widget_show_all(app.mainWindow);
+    g_main_loop_run(ml);
+    g_main_loop_unref(ml);
+    gtk_widget_destroy(app.mainWindow);
     return 0;
 }
